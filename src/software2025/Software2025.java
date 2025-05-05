9870952a7de030e912256bb33254abc1816cf105
sebas123312 <joseenriquesimilox@gmail.com>
2025-05-05 11:09:15 -0600

    Update Software2025.java

--- src/software2025/Software2025.java
+++ src/software2025/Software2025.java
@@ -7,6 +7,7 @@ public class Software2025 {
     public static void main(String[] args) {
         System.out.println("hola");
         System.out.println("hola otra ves");
+        System.out.println("holiss"); 
     }
     
 }
